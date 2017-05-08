9aaf12d8878026efeae17d137f768b1ffe6d026e
rr697 <rr697@cornell.edu>
2017-05-08 16:51:44 -0400

    ldl

---
+++ MCU1/build/default/production/_ext/1472/motor.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1472/motor.o:  \
+../motor.c  \
+../config.h  \
+ ../I2C_Reg_defs.h 
---
+++ MCU1/build/default/production/_ext/1472/motorControl.o.d
@@ -0,0 +1,4 @@
+build/default/production/_ext/1472/motorControl.o:  \
+../motorControl.c  \
+ ../config.h  \
+../I2C_Reg_defs.h 
