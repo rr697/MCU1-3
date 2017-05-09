73a15ea4eb5ea9e24e0619634e4f567cfc5fcd64
rr697 <rr697@cornell.edu>
2017-05-08 20:56:35 -0400

    Update i2c.py
    some changes

--- i2c.py
+++ i2c.py
@@ -3,13 +3,15 @@ import time
 
 bus = smbus.SMBUS(1)
 DEVICE_ADDRESS = 0x1C
-pin = 0x80
+
 
 def writeNumber(DEVICE_ADDRESS, pin, address):
 	bus.write_byte_data(DEVICE_ADDRESS, pin, address)
 	return -1
 
 while True:
+	pin = input("Enter Case:")
 	address = input("Enter Address:")
+	
 	writeNumber(DEVICE_ADDRESS, pin, address)
 time.sleep(0.1)
